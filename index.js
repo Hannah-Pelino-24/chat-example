8475e424a436f5a504287ddd74f0c2042895b326
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2019-09-14 17:12:53 +0800

    update the gamay

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
+var users = []
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
   socket.on('chat message', function (msg) {
-    console.log(msg)
-    socket.broadcast.emit('chat message', msg.username + " : "+msg.message);
+    socket.broadcast.emit('chat message', msg.username + " : " + msg.message);
   });
+  socket.on('online', function (username) {
+    users.push({ id: socket.id, username: username })
+
+  })
+  socket.on('disconnect', function () {
+    for (let i = 0; i < users.length; i++) {
+      if (socket.id == users[i].id) {
+        users.splice(i, 1)
+        break;
+      }
+    }
+  });
+
+  setInterval(function () {
+    socket.emit("online", users)
+  }, 50);
+  console.log(users)
 });
 http.listen(port, function () {
   console.log('listening on *:' + port);
