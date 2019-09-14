0835cf7c7c88ee9e216b47f8456d94345911e596
nanitkatipunan <nanit.katipunan@student.passerellesnumeriques.org>
2019-09-14 20:32:42 +0800

    Updated gamay

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-var users = []
+var port = process.env.PORT || 5555;
+var counter =0;
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
+  socket.on('set-online',function(msg){
+    // counter.push(1)
+    ++counter;
+  })
   socket.on('chat message', function (msg) {
     socket.broadcast.emit('chat message', msg.username + " : " + msg.message);
   });
-  socket.on('online', function (username) {
-    users.push({ id: socket.id, username: username })
-
-  })
-  socket.on('disconnect', function () {
-    for (let i = 0; i < users.length; i++) {
-      if (socket.id == users[i].id) {
-        users.splice(i, 1)
-        break;
-      }
-    }
-  });
-
   setInterval(function () {
-    socket.emit("online", users)
+    socket.emit("online", counter)
   }, 50);
-  console.log(users)
+  socket.on('disconnect',function(){
+    --counter
+  })
 });
 http.listen(port, function () {
   console.log('listening on *:' + port);
